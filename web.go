57fba57a8fd6f6314c5509c7a52fb0da48877d0f
mattn <mattn.jp@gmail.com>
2014-03-06 18:26:57 +0900

    First import

---
+++ web.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World"))
+	})
+	http.ListenAndServe(":"+port, nil)
+}
